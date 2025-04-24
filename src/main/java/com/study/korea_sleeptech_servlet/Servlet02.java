81787fc11df62151eb7727bf9fcbdf0ab5d01762
Yhs-DalC <scvthego@gmail.com>
2025-04-24 10:48:25 +0900

    first commit

---
+++ src/main/java/com/study/korea_sleeptech_servlet/Servlet02.java
@@ -0,0 +1,43 @@
+public class Servlet02 {
+    /*
+    * === 서블릿 프로젝트 생성===
+    * 
+    * cf) intllij ultimate 필요
+    *       : intellij Community Edition은 서블릿/JSP 웹 프로젝트를 지원 X
+    * 
+    * 1. 필수 설치 프로그램
+    *   - intellij ultimate edition
+    *   - jdk17
+    *   - apache tomcat(9.x or 10.x): 자바 11~20은 톰캣 10.x버전 사용
+    * 
+    * 2. 프로젝트 생성
+    * 
+    * 3. 프로젝트 구조
+    *
+    * src(자바소스코드/메인 내부)
+    * 1)java
+    *   :자바 소스 코드 디렉토리
+    *   >> 해당 폴더 내부에 com.study(패키지구조/사용자정의패키지
+    *   >> java 파일들을 생성하고 실질적인 로직 구현
+    *
+    * 2)resources
+    *   : 어플리케이션의 리소스(ex/ properties파일- 프로젝트 세팅 환경을 설정)
+    * 
+    * 3) webapp
+    *   : 웹 어플리케이션의 웹 자원(JSP, Servlet 매핑)
+    *   - 실제 웹 자원이 포함되는 디렉토리
+    * 
+    *   web-inf: 서블릿 매핑을 정의하는 web.xml 파일 존재
+    *   web.xml: 서블릿을 정의하고 매핑하는 설정 파일
+    *       -자바 코드와 웹을 매핑
+    * 
+    *   cf) JSP(자바 서버 페이지)
+    *       : 자바 언어를 사용해 서버 측에서 동적인 웹페이지를 생성하는 기술
+    *       >> HTML코드에 자바 코드를 삽입하여 웹 어플리케이션 개발
+    * 
+    * +) build.graddle
+    *   graddle 프로젝트의 빌드 파일
+    * 
+    * */
+}
+
